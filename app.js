f4f6435b96789ee320d9e21cfb04eecc27fccb26
anis8 <anis.trikiteur@gmail.com>
2020-12-15 01:20:23 +0100

    adding project

---
+++ app.js
@@ -0,0 +1,112 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const {autoUpdater} = require("electron-updater");
+
+const path = require('path');
+const url = require('url');
+
+let pluginName;
+let pluginVersion;
+let mainWindow;
+
+switch (process.platform) {
+    case 'win32':
+        pluginName = 'pepflashplayer.dll';
+        pluginVersion = '20.0.0.306';
+        break;
+    case 'darwin':
+        pluginName = 'PepperFlashPlayer.plugin';
+        pluginVersion = '32.0.0.207';
+        break;
+    case "linux":
+    case "freebsd":
+    case "netbsd":
+    case "openbsd":
+        pluginName = 'libpepflashplayer.so';
+        pluginVersion = '32.0.0.207';
+        break;
+}
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname.includes(".asar") ? process.resourcesPath : __dirname, "flash/" + pluginName));
+
+let createWindow = async () => {
+    mainWindow = new BrowserWindow({
+        title: "HabboCity",
+        icon: __dirname + 'assets/icon.ico',
+        webPreferences: {
+            plugins: true,
+            nodeIntegration: true
+        },
+        show: false,
+        frame: true,
+        backgroundColor: "#000",
+    });
+    mainWindow.maximize();
+    mainWindow.show();
+    mainWindow.setMenu(null);
+
+    await mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, `app.html`),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+
+    mainWindow.webContents.openDevTools();
+
+    ipcMain.on('zoomOut', () => {
+        let factor = mainWindow.webContents.getZoomFactor();
+        if (factor > 0.3) {
+            mainWindow.webContents.setZoomFactor(factor - 0.01);
+        }
+    });
+    ipcMain.on('zoomIn', () => {
+        let factor = mainWindow.webContents.getZoomFactor();
+        if (factor < 3) {
+            mainWindow.webContents.setZoomFactor(factor + 0.01);
+        }
+    });
+};
+
+app.on('ready', async () => {
+    await createWindow();
+    await autoUpdater.checkForUpdatesAndNotify();
+});
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', async () => {
+    if (mainWindow === null) {
+        await createWindow();
+    }
+});
+
+let sendWindow = (identifier, message) => {
+    mainWindow.webContents.send(identifier, message);
+}
+
+autoUpdater.on('checking-for-update', () => {
+    sendWindow('checking-for-update', '');
+});
+autoUpdater.on('update-available', () => {
+    sendWindow('update-available', '');
+});
+autoUpdater.on('update-not-available', () => {
+    sendWindow('update-not-available', '');
+});
+autoUpdater.on('error', (err) => {
+    sendWindow('error', 'Error: ' + err);
+});
+autoUpdater.on('download-progress', (d) => {
+    sendWindow('download-progress', {
+        speed: d.bytesPerSecond,
+        percent: d.percent,
+        transferred: d.transferred,
+        total: d.total
+    });
+})
+autoUpdater.on('update-downloaded', () => {
+    sendWindow('update-downloaded', 'Update downloaded');
+});
